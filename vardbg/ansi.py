ef2cdbbd67253a33a7553689d56331d0837b18fc
Danny Lin <danny@kdrag0n.dev>
2019-12-30 15:46:54 -0800

    vardbg: ansi: Add bold yellow color
    To be used for warnings.

--- vardbg/ansi.py
+++ vardbg/ansi.py
@@ -3,6 +3,7 @@
 RED = "\u001b[1;31m"
 GREEN = "\u001b[1;32m"
 BLUE = "\u001b[1;34m"
+YELLOW = "\u001b[1;33m"
 BOLD = "\u001b[1m"
 RESET = "\u001b[0m"
 
@@ -17,6 +18,10 @@ def green(content):
 
 def blue(content):
     return BLUE + str(content) + RESET
+
+
+def yellow(content):
+    return YELLOW + str(content) + RESET
 
 
 def bold(content):
