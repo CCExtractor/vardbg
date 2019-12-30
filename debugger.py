f577cbad5c75f80973297e91a34296ce3e6212f1
Danny Lin <danny@kdrag0n.dev>
2019-12-29 19:10:11 -0800

    vardbg: Add support for showing changes within containers (II)
    Includes lists, dicts, sets, etc.

--- debugger.py
+++ debugger.py
@@ -18,14 +18,26 @@ def test_func():
     x *= 2
 
     lst = [1]
+    lst.append(x)
+    lst.append(y)
+    del lst[2], lst[1]
+
     st = {1}
+    st.add(x)
+    st.add(y)
+    st.update({1, 2, 3, 4, 5})
+    st.remove(x)
+
+    dct = {"a": 1, "b": 2, "c": 3}
+    dct["d"] = 4
+    del dct["c"]
 
     v1, v2 = 0, 1
     v1, v2 = 1, 2
 
     del v1, v2
 
-    print(y, x, lst, st)
+    print(y, x, lst, st, dct)
 
 
 def debug_func(func):
@@ -49,20 +61,46 @@ def debug_func(func):
         # Diff and print changes
         for action, chg_var, chg in dictdiffer.diff(fn_locals, new_locals):
             if action == dictdiffer.ADD:
-                # chg is a list of tuples with variable names and values
-                for var, val in chg:
-                    print(f"{line} {var} {GREEN}added{RESET} with value {repr(val)}")
+                # If we have a changed variable, elements were added to a list/set/dict
+                if chg_var:
+                    # Get a reference to the container to check its type
+                    container = new_locals[chg_var]
+
+                    # chg is a list of tuples with keys (index, key, etc.) and values
+                    for key, val in chg:
+                        if isinstance(container, set):
+                            # Show it as an extension for sets
+                            print(f"{line} {chg_var} {GREEN}extended{RESET} with {repr(val)}")
+                        else:
+                            # Render it as var[key] for lists, dicts, etc.
+                            print(f"{line} {chg_var}[{repr(key)}] {GREEN}added{RESET} with value {repr(val)}")
+                # Otherwise, it's a new variable
+                else:
+                    # chg is a list of tuples with variable names and values
+                    for var, val in chg:
+                        print(f"{line} {var} {GREEN}added{RESET} with value {repr(val)}")
             elif action == dictdiffer.CHANGE:
+                # If the changed variable is given as a list, a list/set/dict element was changed
+                if isinstance(chg_var, list):
+                    # chg_var is a tuple with the variable name and key
+                    var_name, key = chg_var
+                    # Render it as var_name[key]
+                    chg_var = f"{var_name}[{repr(key)}]"
+
                 # chg_var is the variable that was changed
                 # chg is a tuple with the before and after values
                 before, after = chg
-                print(
-                    f"{line} {chg_var} {BLUE}changed{RESET} from {repr(before)} to {repr(after)}"
-                )
+                print(f"{line} {chg_var} {BLUE}changed{RESET} from {repr(before)} to {repr(after)}")
             elif action == dictdiffer.REMOVE:
-                # chg is a list of tuples with variable names and values
-                for var, val in chg:
-                    print(f"{line} {var} {RED}deleted{RESET} (value: {repr(val)})")
+                # If we have a changed variable, elements were removed from a list/set/dict
+                if chg_var:
+                    for key, val in chg:
+                        print(f"{line} {chg_var}[{repr(key)}] {RED}removed{RESET} (value: {repr(val)})")
+                # Otherwise, a variable was deleted
+                else:
+                    # chg is a list of tuples with variable names and values
+                    for var, val in chg:
+                        print(f"{line} {var} {RED}deleted{RESET} (value: {repr(val)})")
 
         # Update previous locals variable
         fn_locals = new_locals
