5d8e55cd6144e2860735ee9c09041dfd260e3c62
Danny Lin <danny@kdrag0n.dev>
2020-01-12 17:14:16 -0800

    video_writer: Add GIF encoder (IX)

---
+++ vardbg/output/video_writer/gif_encoder.py
@@ -0,0 +1,15 @@
+from .encoder import Encoder
+
+
+class GIFEncoder(Encoder):
+    def __init__(self, path, fps):
+        self.frames = []
+        self.path = path
+        self.fps = fps
+
+    def write(self, image):
+        self.frames.append(image)
+
+    def stop(self):
+        frame_dur = 1000 / self.fps
+        self.frames[0].save(self.path, save_all=True, append_images=self.frames[1:], duration=frame_dur, loop=0)
