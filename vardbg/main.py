fc5e8df26e4cd1653ee24d4cd9e221aac02ce930
Danny Lin <danny@kdrag0n.dev>
2020-01-27 20:59:50 -0800

    main: Reimplement command-line interface using Click

--- vardbg/main.py
+++ vardbg/main.py
@@ -1,63 +1,45 @@
-import argparse
 import importlib.util
 from pathlib import Path
 
+import click
+
 from . import ansi, debugger
 
+DESC = "A simple Python debugger and profiler that can generate animated visualizations of program flow."
 
-def parse_args():
-    parser = argparse.ArgumentParser(
-        description="A simple Python debugger and profiler that can generate animated visualizations of program flow."
-    )
-
-    parser.add_argument("file", metavar="FILE", help="Python file to debug or JSON file to replay")
-    parser.add_argument(
-        "-n",
-        "--function",
-        nargs="?",
-        default="main",
-        help="function to run from the given file (if applicable, default main)",
-    )
-    parser.add_argument(
-        "-o",
-        "--output-file",
-        nargs="?",
-        metavar="OUTPUT",
-        help="path to write JSON output file to (will be truncated/created if necessary)",
-    )
-    parser.add_argument(
-        "-v",
-        "--video",
-        nargs="?",
-        help="path to write a video representation of the program flow to (supports MP4, GIF, and WebP formats based on file extension)",
-    )
-    parser.add_argument(
-        "-c", "--video-config", nargs="?", metavar="CONFIG", help="path to the TOML video output config",
-    )
-    parser.add_argument(
-        "-a", "--args", nargs="*", default=[], metavar="ARGS", help="list of arguments to pass to the running program",
-    )
-    parser.add_argument(
-        "-p",
-        "--absolute-paths",
-        default=False,
-        action="store_true",
-        help="use absolute paths instead of relative ones",
-    )
-    parser.add_argument(
-        "-P",
-        "--disable-live-profiler",
-        default=False,
-        action="store_true",
-        help="disable live profiler output during execution",
-    )
-
-    return parser.parse_args()
+VIDEO_HELP = (
+    "Write a video representation of the program flow (supports MP4, GIF, and WebP formats based on file extension)."
+)
+VIDEO_CONFIG_HELP = "TOML video config overlay to load."
 
 
-def do_debug(args, mod):
+@click.group(help=DESC, context_settings={"help_option_names": ("-h", "--help")})
+def cli():
+    pass
+
+
+@cli.command(help="Run and debug the given Python file.")
+@click.argument("file")
+@click.argument("function", required=False, default="main")
+@click.option("-a", "--arguments", multiple=True, metavar="ARGS", help="Arguments to pass to the program.")
+@click.option("-o", "--output", metavar="PATH", help="Write a JSON session recording.")
+@click.option("-v", "--video", metavar="PATH", help=VIDEO_HELP)
+@click.option("-c", "--video-config", metavar="PATH", help=VIDEO_CONFIG_HELP)
+@click.option(
+    "-p", "--absolute-paths", default=False, is_flag=True, help="Use absolute paths instead of relative ones."
+)
+@click.option(
+    "-P", "--disable-live-profiler", default=False, is_flag=True, help="Disable live profiler output during execution.",
+)
+def run(file, function, arguments, output, video, video_config, absolute_paths, disable_live_profiler):
+    # Load file as module
+    mod_name = Path(file).stem
+    spec = importlib.util.spec_from_file_location(mod_name, file)
+    mod = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(mod)
+
     # Get the function here regardless of which path we took above
-    func = getattr(mod, args.function, None)
+    func = getattr(mod, function, None)
     if func is None:
         # Check how many functions are present in the module
         func_syms = [sym for sym in dir(mod) if callable(getattr(mod, sym))]
@@ -65,43 +47,32 @@ def do_debug(args, mod):
             # Safe to assume that the user wanted this one if it's the only one
             f_sym = func_syms[0]
             func = getattr(mod, f_sym)
-            print(ansi.yellow(f"Unable to find function '{args.function}', falling back to the only one: '{f_sym}'"))
+            print(ansi.yellow(f"Unable to find function '{function}', falling back to the only one: '{f_sym}'"))
             print()
         else:
             # Ambiguous if multiple, so bail out and let the user choose
-            print(ansi.red(f"Unable to find function '{args.function}' and multiple are present; aborting."))
+            print(ansi.red(f"Unable to find function '{function}' and multiple are present; aborting."))
             return 1
 
     # Call the actual debugger with our parameters
     debugger.debug(
         func,
-        args=[args.file or func.__code__.co_filename, *args.args],
-        relative_paths=not args.absolute_paths,
-        json_out_path=args.output_file,
-        video_out_path=args.video,
-        video_config=args.video_config,
-        live_profiler_output=not args.disable_live_profiler,
+        args=[file or func.__code__.co_filename, *arguments],
+        relative_paths=not absolute_paths,
+        json_out_path=output,
+        video_out_path=video,
+        video_config=video_config,
+        live_profiler_output=not disable_live_profiler,
     )
-    return 0
 
 
-def do_replay(args):
-    debugger.replay(args.file, video_out_path=args.video, video_config=args.video_config)
+@cli.command(help="Replay the given JSON session recording. The original source files must still be accessible.")
+@click.argument("file")
+@click.option("-v", "--video", metavar="PATH", help=VIDEO_HELP)
+@click.option("-c", "--video-config", metavar="PATH", help=VIDEO_CONFIG_HELP)
+def replay(file, video, video_config):
+    debugger.replay(file, video_out_path=video, video_config=video_config)
 
 
 def main():
-    args = parse_args()
-
-    # Use pathlib to get more info about the input file
-    file_path = Path(args.file)
-    if file_path.suffix == ".json":
-        # JSON file means to replay, not debug
-        return do_replay(args)
-    else:
-        # Load file as module and debug
-        mod_name = Path(args.file).stem
-        spec = importlib.util.spec_from_file_location(mod_name, args.file)
-        mod = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(mod)
-
-        return do_debug(args, mod)
+    cli()
