CCExtractor/vardbg

commit 561252ac827dc96489c0bd6ade198a50b6493430

profiler: Use high-precision clocks with Python < 3.7 fallbacks
- Use performance counter on Windows and process time on other platforms for maximum precision (according to PEP-564 [1])
- Support Python versions older than 3.7 by adding a wrapper to simulate
  nanosecond precision when the direct nanosecond getters are
  unavailable

Fixes #1.

[1] https://www.python.org/dev/peps/pep-0564/#annex-clocks-resolution-in-python

vardbg/profiler.py
@@ -1,4 +1,5 @@
 import abc
+import platform
 import time
 from typing import TYPE_CHECKING
 
@@ -8,6 +9,26 @@ from . import internal
 
 if TYPE_CHECKING:
     from .debugger import Debugger
+
+
+# Helper to call a function from the time module with nanosecond precision if possible, and lower prrcision otherwise
+def _time_wrap_ns(sym_base):
+    sym_ns = sym_base + "_ns"
+    if hasattr(time, sym_ns):
+        # Use ns function if possible
+        return getattr(time, sym_ns)
+    else:
+        # Otherwise, create a wrapper to convert it
+        # This is necessary on Python < 3.7
+        func = getattr(time, sym_base)
+        return lambda: int(func() * 1_000_000_000)
+
+
+# Use performance counter on Windows and process time on others for max precision (ref: PEP-564)
+if platform.system() == "Windows":
+    get_time_ns = _time_wrap_ns("perf_counter")
+else:
+    get_time_ns = _time_wrap_ns("process_time")
 
 
 class Profiler(abc.ABC):
@@ -25,10 +46,10 @@ class Profiler(abc.ABC):
         super().__init__()
 
     def profile_start_frame(self: "Debugger"):
-        self.prev_frame_start_time = time.time_ns()
+        self.prev_frame_start_time = get_time_ns()
 
     def profile_complete_frame(self: "Debugger", prev_frame_info):
-        exec_time = time.time_ns() - self.prev_frame_start_time
+        exec_time = get_time_ns() - self.prev_frame_start_time
 
         if prev_frame_info in self.frame_exec_times:
             self.frame_exec_times[prev_frame_info].append(exec_time)

commit 5a4d53f0cc53cab0ec1ba0aeeae17bdda5243467

profiler: Always use performance counter for accurate timing
Process time is inaccurate because it only includes time spent inside
the process. For example, time.sleep(1) is counted as mere microseconds
because the time spent outside the process is ignored.

Fixes: 561252a ("profiler: Use high-precision clocks with Python < 3.7 fallbacks")

vardbg/profiler.py
@@ -1,5 +1,4 @@
 import abc
-import platform
 import time
 from typing import TYPE_CHECKING
 
@@ -11,7 +10,7 @@ if TYPE_CHECKING:
     from .debugger import Debugger
 
 
-# Helper to call a function from the time module with nanosecond precision if possible, and lower prrcision otherwise
+# Helper to call a function from the time module with nanosecond precision if possible, and lower precision otherwise
 def _time_wrap_ns(sym_base):
     sym_ns = sym_base + "_ns"
     if hasattr(time, sym_ns):
@@ -24,11 +23,8 @@ def _time_wrap_ns(sym_base):
         return lambda: int(func() * 1_000_000_000)
 
 
-# Use performance counter on Windows and process time on others for max precision (ref: PEP-564)
-if platform.system() == "Windows":
-    get_time_ns = _time_wrap_ns("perf_counter")
-else:
-    get_time_ns = _time_wrap_ns("process_time")
+# Use performance counter for max precision (ref: PEP-564)
+get_time_ns = _time_wrap_ns("perf_counter")
 
 
 class Profiler(abc.ABC):