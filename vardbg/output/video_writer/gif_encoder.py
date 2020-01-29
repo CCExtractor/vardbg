bd10cb9c57144f9a20b8fa00d3681b348232e9d7
Danny Lin <danny@kdrag0n.dev>
2020-01-28 21:40:17 -0800

    video_writer: gif_encoder: Improve GIF quality using imageio writer
    This results in much better quality than Pillow's animated GIF output.
    The size is also much lower (34 MB -> 2.3 MB for the insertion sort
    demo) due to subrectangle optimizations.

--- vardbg/output/video_writer/gif_encoder.py
+++ vardbg/output/video_writer/gif_encoder.py
@@ -1,18 +1,15 @@
+import imageio
+import numpy as np
+
 from .encoder import Encoder
 
 
 class GIFEncoder(Encoder):
     def __init__(self, path, fps):
-        self.frames = []
-        self.path = path
-        self.fps = fps
+        self.writer = imageio.get_writer(path, mode="I", fps=fps, subrectangles=True)
 
     def write(self, image):
-        self.frames.append(image)
-
-    def save(self, frame_dur):
-        self.frames[0].save(self.path, save_all=True, append_images=self.frames[1:], duration=frame_dur, loop=0)
+        self.writer.append_data(np.array(image))
 
     def stop(self):
-        frame_dur = 1000 / self.fps
-        self.save(frame_dur)
+        self.writer.close()
