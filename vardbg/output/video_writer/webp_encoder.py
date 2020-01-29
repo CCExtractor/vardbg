0cf1ba3027d23829e7458c2bb257b672235da1e6
Danny Lin <danny@kdrag0n.dev>
2020-01-28 21:23:01 -0800

    video_writer: Make WebP encoder independent of GIF encoder
    The GIF encoder is about to be revamped.

--- vardbg/output/video_writer/webp_encoder.py
+++ vardbg/output/video_writer/webp_encoder.py
@@ -1,8 +1,16 @@
-from .gif_encoder import GIFEncoder
+from .encoder import Encoder
 
 
-class WebPEncoder(GIFEncoder):
+class WebPEncoder(Encoder):
+    def __init__(self, path, fps):
+        self.frames = []
+        self.path = path
+        self.fps = fps
+
+    def write(self, image):
+        self.frames.append(image)
+
     def stop(self):
         # WebP requires an integer duration
         frame_dur = round(1000 / self.fps)
-        self.save(frame_dur)
+        self.frames[0].save(self.path, save_all=True, append_images=self.frames[1:], duration=frame_dur, loop=0)
